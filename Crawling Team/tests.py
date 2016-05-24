01098d3a5a96605dc41693d522f6a67c597dfd50
Mihai Nechita <mnechita@bitdefender.com>
2016-05-24 10:57:31 +0300

    fixed tests

--- Crawling Team/tests.py
+++ Crawling Team/tests.py
@@ -11,12 +11,17 @@ class CrawlerTest(unittest.TestCase):
         self.assertEqual(u'590.000', content['pret']['valoare'])
         self.assertTrue(u'Apa' in content['dotari']['Utilitati:'])
 
+    def test_process_content2(self):
+        with io.open(pjoin('tests', 'ap2camereTatarasi.json'), 'r', encoding=encoding) as fd:
+            sample_json = json.load(fd)
+            processed_json = process_content('file:' + abspath(pjoin('tests', 'ap2camereTatarasi.html')).replace('\\', '/'))
+            self.assertEqual(cmp(sample_json, processed_json), 0)
 
     def test_process_html_tags(self):
         content = '<td><div>text</div></td>'
         self.assertEqual(process_html_tags(content), 'text')
 
-    def test_process_html_tags(self):
+    def test_process_html_tags2(self):
         content = '<td><tr><div><a href="google.com" >test &nbsp;</a></div><tr</td>'
         self.assertEqual(process_html_tags(content), 'test')
 
@@ -25,14 +30,12 @@ class CrawlerTest(unittest.TestCase):
             self.assertEqual(fd.read(), get_page_content('file:' + abspath(pjoin('tests', 'acf.html')).replace('\\', '/')))
 
     def test_get_page_content2(self):
-        with io.open(pjoin('tests', 'ap2camereTatarasi.html'), 'r', encoding=encoding) as fd:
-            self.assertEqual(fd.read(), get_page_content('file:' + abspath(pjoin('tests', 'ap2camereTatarasi.html')).replace('\\', '/')))
+        with io.open(pjoin('tests', 'ap2camereTatarasi.html'), 'rb') as fd:
+            self.assertEqual(fd.read().decode(encoding), get_page_content('file:' + abspath(pjoin('tests', 'ap2camereTatarasi.html')).replace('\\', '/')))
 
-    def test_process_content2(self):
-        with io.open(pjoin('tests', 'ap2camereTatarasi.json'), 'r', encoding=encoding) as fd:
-            sample_json = json.load(fd)
-            processed_json = process_content('file:' + abspath(pjoin('tests', 'ap2camereTatarasi.html')).replace('\\', '/'))
-            self.assertEqual(cmp(sample_json, processed_json), 0)
+    def test_get_page_content3(self):
+        with io.open(pjoin('tests', 'casavilabucium.html'), 'rb') as fd:
+            self.assertEqual(fd.read().decode(encoding), get_page_content('file:' + abspath(pjoin('tests', 'casavilabucium.html')).replace('\\', '/')))
 
 
 if __name__ == '__main__':
